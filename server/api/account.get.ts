e4585b911e192130035a8725d4309ecefa3e2b25
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-10-01 23:00:12 -0500

    dummy api

--- server/api/account.get.ts
+++ server/api/account.get.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
-	const { RIOT_API_KEY } = useRuntimeConfig(event)
+	const { RIOT_API_KEY } = useRuntimeConfig()
 	const query = getQuery(event)
 	const gameName = query.gameName as string
 	const tagLine = query.tagLine as string
